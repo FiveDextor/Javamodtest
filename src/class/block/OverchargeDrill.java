9dd9915096070347dffc61717376d3dab0dd7aaa
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-20 22:23:59 +0700

    Create OverchargeDrill.java

---
+++ src/class/block/OverchargeDrill.java
@@ -0,0 +1,27 @@
+package; testDEX
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
+import arc.util.io.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.units.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.consumers.*;
+import mindustry.world.meta.*;
+
+public class OverchargeDrill extends Drill{
+  public int maxCharge = 3
+}
