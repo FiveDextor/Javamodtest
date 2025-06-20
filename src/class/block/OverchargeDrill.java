3a497a68fc3815923714086acc271434200a26d7
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-21 00:13:24 +0700

    Update OverchargeDrill.java

--- src/class/block/OverchargeDrill.java
+++ src/class/block/OverchargeDrill.java
@@ -1,23 +1,16 @@
-package testdex;
+package mindustry.world.blocks.production;
 
-import arc.*;
+import arc.audio.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
-import arc.struct.*;
 import arc.util.*;
-import arc.util.io.*;
+import mindustry.annotations.Annotations.*;
 import mindustry.content.*;
 import mindustry.entities.*;
-import mindustry.entities.units.*;
-import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
-import mindustry.logic.*;
 import mindustry.type.*;
-import mindustry.ui.*;
-import mindustry.world.*;
-import mindustry.world.blocks.environment.*;
 import mindustry.world.consumers.*;
 import mindustry.world.meta.*;
 
