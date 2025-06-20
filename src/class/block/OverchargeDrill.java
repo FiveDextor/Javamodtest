88624304eaf1619a6c55163f5372d0ba661ec75a
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-20 22:34:50 +0700

    Update OverchargeDrill.java

--- src/class/block/OverchargeDrill.java
+++ src/class/block/OverchargeDrill.java
@@ -26,7 +26,7 @@ public class OverchargeDrill extends Drill{
   /** Max charge of drill before overcharing */
   public float maxCharge = 10;
   /** Chance to gain charge from producing an item */
-  public float chargeChance;
+  public float chargeChance = 1;
   public float overchargeDrillTime = 150;
   public float overchargeTime = 6000;
   public float overchargeRecover = 3000;
