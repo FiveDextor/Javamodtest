1f037774b5c5a6f0e8b734c99e0ac438df82dd3c
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-23 13:17:34 +0700

    Update ChargeTurret.java

--- src/testdex/block/ChargeTurret.java
+++ src/testdex/block/ChargeTurret.java
@@ -3,5 +3,7 @@ package testdex.block;
 import mindustry.world.blocks.defense.turrets.ItemTurret;
 
 public class ChargeTurret extends ItemTurret {
-  
+  public ChargeTurret(String name) {
+        super(name);
+  }
 }
