d7c5431a95b220b0a710b5a2562fd2b27c3eb7e0
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-23 15:53:14 +0700

    Update OverchargeDrill.java

--- src/testdex/block/OverchargeDrill.java
+++ src/testdex/block/OverchargeDrill.java
@@ -186,7 +186,7 @@ public class OverchargeDrill extends Drill {
 
             if (drawSpinSprite) {
                 float progress = windUp / windUpTime;        // from 0 to 1 over 60 ticks
-                progress = Mathf.Clamp01(progress);          // ensures [0,1]
+                progress = Mathf.clamp(progress);          // ensures [0,1]
                 float multiplier = Mathf.lerp(1f, 2f, progress);
             if (isOvercharging) {
                 Drawf.spinSprite(rotatorRegion, x, y,
