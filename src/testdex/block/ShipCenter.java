ab09f5d44054d1a151d506c5e7795d86b0cf18d6
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-24 11:06:16 +0700

    Update and rename BuildingShipCenter.java to ShipCenter.java

---
+++ src/testdex/block/ShipCenter.java
@@ -0,0 +1,9 @@
+package testdex.block;
+
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+
+public class ShipCenter extends ItemTurret {
+  public ShipCenter(String name) {
+        super(name);
+  }
+}
