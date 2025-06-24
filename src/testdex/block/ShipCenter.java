a98d4bb4e500bb6d309523ad0c4066c80e8f2ebc
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-24 11:31:50 +0700

    Update ShipCenter.java

--- src/testdex/block/ShipCenter.java
+++ src/testdex/block/ShipCenter.java
@@ -8,6 +8,40 @@ public class ShipCenter extends UnitCargoLoader{
         size = 2;
   }
   public class UnitPayloadSourceBuild extends UnitTransportSourceBuild{
-    
+    @Override
+        public void updateTile(){
+            //unit was lost/destroyed
+            if(unit != null && (unit.dead || !unit.isAdded())){
+                unit = null;
+            }
+
+            if(readUnitId != -1){
+                unit = Groups.unit.getByID(readUnitId);
+                if(unit != null || !net.client()){
+                    readUnitId = -1;
+                }
+            }
+
+            warmup = Mathf.approachDelta(warmup, efficiency, 1f / 60f);
+            readyness = Mathf.approachDelta(readyness, unit != null ? 1f : 0f, 1f / 60f);
+
+            if(unit == null && Units.canCreate(team, unitType)){
+                buildProgress += edelta() / unitBuildTime;
+                totalProgress += edelta();
+
+                if(buildProgress >= 1f){
+                    if(!net.client()){
+                        unit = unitType.create(team);
+                        if(unit instanceof BuildingTetherc bt){
+                            bt.building(this);
+                        }
+                        unit.set(x, y);
+                        unit.rotation = 90f;
+                        unit.add();
+                        Call.unitTetherBlockSpawned(tile, unit.id);
+                    }
+                }
+            }
+        }
   }
 }
