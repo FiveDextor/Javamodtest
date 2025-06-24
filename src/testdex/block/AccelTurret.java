366d96766c7b7ba6e8e0ce37e23e8f1c4f8c8dd8
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-24 16:42:14 +0700

    Update AccelTurret.java

--- src/testdex/block/AccelTurret.java
+++ src/testdex/block/AccelTurret.java
@@ -3,12 +3,14 @@ package testdex.block;
 import mindustry.world.blocks.defense.turrets.ItemTurret;
 
 public class AccelTurret extends ItemTurret {
+  public float charge = 0;
+  public float maxCharge = 60;
+  public float multiplier = 2;
+  
   public AccelTurret(String name) {
         super(name);
   }
   public class ItemTurretBuild extends TurretBuild{
-    public float charge = 0;
-    public float maxCharge = 60;
-    public float multiplier = 2;
+
   }
 }
