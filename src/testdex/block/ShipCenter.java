122991ca2b6fc7b7be213695d58356d7b5a05e8b
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-24 11:07:40 +0700

    Update ShipCenter.java

--- src/testdex/block/ShipCenter.java
+++ src/testdex/block/ShipCenter.java
@@ -5,5 +5,6 @@ import mindustry.world.blocks.defense.turrets.ItemTurret;
 public class ShipCenter extends ItemTurret {
   public ShipCenter(String name) {
         super(name);
+        size = 2;
   }
 }
