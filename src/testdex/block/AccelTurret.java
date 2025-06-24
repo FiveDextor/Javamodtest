31232817f73c164c0c30137bac9515682d15040a
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-24 17:23:26 +0700

    Update AccelTurret.java

--- src/testdex/block/AccelTurret.java
+++ src/testdex/block/AccelTurret.java
@@ -4,28 +4,26 @@ import mindustry.world.blocks.defense.turrets.ItemTurret;
 import arc.math.Mathf;
 
 public class AccelTurret extends ItemTurret {
-  public float maxCharge = 60;
-  public float multiplier = 1;
-  
-  public AccelTurret(String name) {
+    public float maxCharge = 60;
+    public float multiplier = 1;
+
+    public AccelTurret(String name) {
         super(name);
-  }
-  public class ItemTurretBuild extends TurretBuild{
-  public float charge = 0;
+    }
 
-  @Override
-  protected void updateReload(){
+    public class ItemTurretBuild extends TurretBuild {
+        public float charge = 0;
+
+        @Override
+        protected void updateReload() {
             reloadCounter += delta() * ammoReloadMultiplier() * baseReloadSpeed();
-
             //cap reload for visual reasons
-            reloadCounter = Math.min(reloadCounter,reload / (1 + multiplier * charge / maxCharge));
-     }
-  }
+            reloadCounter = Math.min(reloadCounter, reload / (1 + multiplier * charge / maxCharge));
+        }
 
-  @Override
-  protected void updateShooting(){
-
-            if(reloadCounter >= reload / (1 + multiplier * charge / maxCharge) && !charging() && shootWarmup >= minWarmup){
+        @Override
+        protected void updateShooting() {
+            if (reloadCounter >= reload / (1 + multiplier * charge / maxCharge) && !charging() && shootWarmup >= minWarmup) {
                 BulletType type = peekAmmo();
 
                 shoot(type);
@@ -33,5 +31,6 @@ public class AccelTurret extends ItemTurret {
                 reloadCounter %= reload;
                 charge += 1;
             }
-  }
+        }
+    }
 }
