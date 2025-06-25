d009d62e115aa23b363c080ab7b2fbe307bda2be
Dextor <160398952+FiveDextor@users.noreply.github.com>
2025-06-25 08:28:17 +0700

    Update DextorJavaTest.java

--- src/testdex/DextorJavaTest.java
+++ src/testdex/DextorJavaTest.java
@@ -54,18 +54,20 @@ public class DextorJavaTest extends Mod{
         }};
         AccelTurret customAccelTurret = new AccelTurret("accel-turret"){{
             requirements(Category.turret, ItemStack.with());
-            maxCharge = 15;
             ammo(
             Items.graphite, new BasicBulletType(8f, 41){{
                 knockback = 4f;
                 width = 25f;
                 hitSize = 7f;
                 height = 20f;
+                shootEffect = Fx.shootBigColor;
+                smokeEffect = Fx.shootSmokeSquareSparse;
                 ammoMultiplier = 1;
                 hitColor = backColor = trailColor = Color.valueOf("ea8878");
-                frontColor = Color.valueOf("ff0000");
+                frontColor = Pal.redLight;
                 trailWidth = 6f;
                 trailLength = 3;
+                hitEffect = despawnEffect = Fx.hitSquaresColor;
                 buildingDamageMultiplier = 0.2f;
             }},
             Items.oxide, new BasicBulletType(8f, 120){{
@@ -73,11 +75,14 @@ public class DextorJavaTest extends Mod{
                 width = 25f;
                 hitSize = 7f;
                 height = 20f;
+                shootEffect = Fx.shootBigColor;
+                smokeEffect = Fx.shootSmokeSquareSparse;
                 ammoMultiplier = 2;
                 hitColor = backColor = trailColor = Color.valueOf("a0b380");
                 frontColor = Color.valueOf("e4ffd6");
                 trailWidth = 6f;
                 trailLength = 3;
+                hitEffect = despawnEffect = Fx.hitSquaresColor;
                 buildingDamageMultiplier = 0.2f;
             }},
             Items.silicon, new BasicBulletType(8f, 35){{
@@ -86,11 +91,14 @@ public class DextorJavaTest extends Mod{
                 hitSize = 7f;
                 height = 20f;
                 homingPower = 0.045f;
+                shootEffect = Fx.shootBigColor;
+                smokeEffect = Fx.shootSmokeSquareSparse;
                 ammoMultiplier = 1;
-                hitColor = backColor = trailColor = Color.valueOf("a0b380");
-                frontColor = Color.valueOf("e4ffd6");
+                hitColor = backColor = trailColor = Pal.graphiteAmmoBack;
+                frontColor = Pal.graphiteAmmoFront;
                 trailWidth = 6f;
                 trailLength = 6;
+                hitEffect = despawnEffect = Fx.hitSquaresColor;
                 buildingDamageMultiplier = 0.2f;
             }}
             );
@@ -98,9 +106,7 @@ public class DextorJavaTest extends Mod{
             shoot = new ShootSpread(15, 4f);
 
             coolantMultiplier = 15f;
-            
-            maxCharge = 30;
-            multiplier = 4;
+
             inaccuracy = 0.2f;
             velocityRnd = 0.17f;
             shake = 1f;
@@ -109,7 +115,19 @@ public class DextorJavaTest extends Mod{
             consumeAmmoOnce = true;
             targetUnderBlocks = false;
 
+            shootSound = Sounds.shootAltLong;
+
+            drawer = new DrawTurret("reinforced-"){{
+                parts.add(new RegionPart("-front"){{
+                    progress = PartProgress.warmup;
+                    moveRot = -10f;
+                    mirror = true;
+                    moves.add(new PartMove(PartProgress.recoil, 0f, -3f, -5f));
+                    heatColor = Color.red;
+                }});
+            }};
             shootY = 5f;
+            outlineColor = Pal.darkOutline;
             size = 3;
             reload = 30f;
             recoil = 2f;
@@ -117,6 +135,9 @@ public class DextorJavaTest extends Mod{
             shootCone = 40f;
             scaledHealth = 210;
             rotateSpeed = 3f;
+
+            coolant = consume(new ConsumeLiquid(Liquids.water, 15f / 60f));
+            limitRange(25f);
         }};
     }
 }
